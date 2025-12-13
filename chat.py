fcdfb37031b02e0d8cf924787c5e54bd7f028915
anki8769 <167672152+anki8769@users.noreply.github.com>
2025-12-13 15:07:07 +0530

    taking input on browser

--- chat.py
+++ chat.py
@@ -1,9 +1,19 @@
-from flask import Flask 
+from flask import Flask, request 
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Welcome to the Chat Application!"
-
+@app.route("/", methods=["GET", "POST"])
+def chat():
+    if request.method == "POST":
+        user_message = request.form.get("message")
+        print("user said:", user_message)
+        return "Message received"
+    
+    return """
+             <h2>Simple Chat Test</h2>
+        <form method="post">
+            <input type="text" name="message" placeholder="Type something">
+            <button type="submit">Send</button>
+        </form>
+    """
 app.run(debug=True)
